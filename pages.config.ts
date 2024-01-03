e59ce95780b6b9dc612e11bbaf8f4f0c3c6c6fa9
sunpm <isunpm@qq.com>
2024-01-03 16:07:40 +0800

    升级 @uni-helper/vite-plugin-uni-pages，修复类型错误

--- pages.config.ts
+++ pages.config.ts
@@ -1,8 +1,6 @@
 import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
 
 export default defineUniPages({
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
   easycom: {
     // 注意一定要放在custom里，否则无效，https://ask.dcloud.net.cn/question/131175
     custom: {
