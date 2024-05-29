dd3a9c5bdb1fbc50fe9f9bba5facc1a694481ca9
SunPM <isunpm@qq.com>
2024-05-29 14:49:48 +0800

    chore: update deps

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,14 +6,3 @@ declare module '*.vue' {
   const component: DefineComponent<NonNullable<unknown>, NonNullable<unknown>, any>
   export default component
 }
-
-declare module 'uview-plus' {
-  import v3 from '@ttou/uview-typings/v3'
-
-  export default v3.GlobalComponents
-}
-
-// 重写 u--form ref 类型
-declare interface _FormRef extends _FormRef {
-  validateField: (value, cb?: ((errorsRes) => any)) => any
-}
