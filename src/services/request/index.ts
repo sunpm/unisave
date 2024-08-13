512f697d114b2ed151cb9da53ee5a90590fc5a43
SunPM <isunpm@qq.com>
2024-08-13 17:23:58 +0800

    fix: 修改请求文档注释说明

--- src/services/request/index.ts
+++ src/services/request/index.ts
@@ -9,9 +9,16 @@ export * from './helper'
  * @see 文档：https://uni-network.netlify.app/
  */
 export const instance = un.create({
-  // h5 需要添加代理，有需要可自行判断环境后处理
+  // TODO h5 需要添加代理，根据业务需求自行判断环境后处理
   baseUrl: getHttpUrl(),
-  // 统一请求方式 POST，后续可以在请求中传递 config 修改
+  /**
+   * TODO 由于小程序兼容性问题，统一请求方式 POST，根据业务需求在请求中传递 config 修改
+   * 例如：request({
+   *   config: {
+   *     method: 'GET'
+   *   }
+   * })
+   */
   method: 'POST',
   headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
   data: {},
@@ -36,7 +43,7 @@ instance.interceptors.response.use((response: any) => {
     return Promise.reject(response)
   }
   loading.hide(response.config.loading)
-  // 返回数据根据业务需求修改
+  // TODO 返回数据根据业务需求修改
   const { code, data, msg } = response.data
   if (code === 1)
     return data
@@ -52,7 +59,11 @@ instance.interceptors.response.use((response: any) => {
 })
 
 type RequestData = Record<string, any>
-// 预设的请求数据类型和响应数据类型
+
+/**
+ * 预设的请求数据类型和响应数据类型
+ * 泛型内数据类型：<响应数据类型, 请求数据类型>，可不传，默认为 <Record<string, any>, Record<string, any>>
+ */
 export async function request<T, D = RequestData>(
   url: string,
   data?: D,
